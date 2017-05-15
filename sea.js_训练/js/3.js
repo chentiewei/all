cb65f787a0f812b73426c5748fe42a2a73c5d4e6
your namechentiewei <chentiewei123@163.com>
2017-05-15 15:24:20 +0800

    sea.js的学习记录

---
+++ sea.js_训练/js/3.js
@@ -0,0 +1,7 @@
+define([], function() {
+  return {
+    c: function() {
+      return 3;
+    }
+  }
+});
---
+++ sea.js_训练/js/main.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+  var $ = require('jquery');
+  var j1=require('1')
+  exports.sayHello = function() {
+    console.log('sayhttlo',j1.a());
+
+  };
+});
