afa11f83455006d08205bd033331777a6423bae1
chentiewei <chentiewei147@163.com>
2017-08-01 11:11:20 +0800

    ctw 完成爬虫

--- node_训练/node_2/index.js
+++ node_训练/node_2/index.js
@@ -1,10 +1,32 @@
 var express=require('express');
+var superagent=require('superagent');
+var cheerio = require('cheerio');
 var app=express();
 
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
-    res.send('hellow');
+    superagent.get('https://cnodejs.org/')
+        .end(function(err,sra){
+            if(err){
+                return next(err);
+            }
+            var $ = cheerio.load(sra.text);
+            var items=[];
+            $('#topic_list .topic_title').each(function(i,p){
+                var item=$(p);
+                items.push({
+                    title:item.attr('title'),
+                    href:item.attr('href'),
+                    author:item.attr('author')
+                })
+            })
+            $('#topic_list .user_avatar img').each(function(i,p){
+                items[i].author=$(p).attr('title');
+            })
+            res.send(items);
+        })
+
 })
 
 app.get('/loging',function(req,res){
@@ -14,6 +36,18 @@ app.get('/loging',function(req,res){
 
 app.set('foo', true);
 console.log(app.get('foo'));
+
+app.use(function(req,res,next){
+    console.log('111');
+    next();
+    console.log('222');
+});
+
+app.use(function(req,res,next){
+    console.log("333");
+    next();
+});
+
 
 var server=app.listen(8088, function () {
     var host = server.address().address;
