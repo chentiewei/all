c39528c37c5ada40b0e92ea8f67f9b58e390d14a
chentiewei <chentiewei147@163.com>
2017-08-14 15:43:03 +0800

    ctw layui的改变

--- sundry/lay_训练/js/api.js
+++
@@ -1,6 +0,0 @@
-layui.define(function(exports){
-    var api=function(){
-        console.log('api')
-    }
-    exports('api', api);
-});
--- sundry/lay_训练/js/layui.config.js
+++ sundry/lay_训练/js/layui.config.js
@@ -1,4 +1,4 @@
 layui.config({
-    base: '../js/',//你的模块目录
+    base: './',//你的模块目录
     version: false
 }).use('index'); //加载入口
