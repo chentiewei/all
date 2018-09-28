0efefdc764b444c2f2460ec09a6813daa0012225
ProjectXX-PC\Project XX <15957768088@163.com>
2018-09-28 17:24:17 +0800

    no message

--- react_训练/react1/src/App.js
+++ react_训练/react1/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          哈哈哈婚纱哈哈哈哈哈十三测试诶
+          这是一个悲伤的故事谁都猜不到应该怎么办，学习react是在一个夜黑风高的夜晚，希望大家谨慎学习，master
       </div>
     );
   }
