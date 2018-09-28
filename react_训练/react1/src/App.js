c8e354ba4148de2b156f0520a641492824ebd4ea
ProjectXX-PC\Project XX <15957768088@163.com>
2018-09-28 17:18:42 +0800

    react 正式学习

---
+++ react_训练/react1/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          哈哈哈婚纱哈哈哈哈哈十三测试诶
+      </div>
+    );
+  }
+}
+
+export default App;
